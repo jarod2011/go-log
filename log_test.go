1ffa9f6449b39bffca37a9d7e2b04b1267fab64d
Jarod <xinghen249@gmail.com>
2020-04-26 10:41:01 +0800

    Update log_test.go

--- log_test.go
+++ log_test.go
@@ -2,20 +2,33 @@ package go_log_test
 
 import (
 	log "github.com/jarod2011/go-log"
+	"sync/atomic"
 	"testing"
 	"time"
 )
 
 var timeStart time.Time
+var count uint32
 
 func TestFlog_Log(t *testing.T) {
 	log.SetLevel(log.Info)
 	timeStart = time.Now()
 	t1()
+	t.Log(count)
+	if count != 8 {
+		t.Errorf("count should equal 8 but %d not", count)
+	}
+	t.Logf("all used %v", time.Since(timeStart))
 }
 
 func t1() {
+	defer func() {
+		if err := recover(); err != nil {
+			log.E().Logf("recover %v", err)
+		}
+	}()
 	for i := 0; i < 10; i++ {
+		atomic.AddUint32(&count, 1)
 		t2(i)
 		time.Sleep(time.Millisecond * 5)
 	}
@@ -34,4 +47,11 @@ func t2(index int) {
 		log.F().Logf("break at %d", index)
 	}
 	log.D().Logf("index %d", index)
+}
+
+func BenchmarkLog(b *testing.B) {
+	log.SetLevel(log.Info)
+	for i := 0; i < b.N; i ++ {
+		log.D().Log(i)
+	}
 }
